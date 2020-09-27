23d17054d3b3f7736a0b22bf2dfe6043c811eae4
Kay <69727381+hallokay@users.noreply.github.com>
2020-09-27 23:14:13 +0900

    0927
    drink
    js error message

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ const switchV = document.querySelector('.v_switch'),
     switchBtn =document.querySelector('.v_switch_btn'),
      bgV = document.querySelector('.video');
 
-const drinkForm = document.querySelector('.drink_form');
+     const drinkForm = document.querySelector('.drink_form');
+let feedBack = document.querySelector('.drink_form_feedback');
+
 
     //  function class
 class UI {
@@ -32,15 +34,47 @@ class UI {
         };  
     };
 
-    freeDrink(){
-   const name = document.querySelector('.input_name').value,
-        lastName = document.querySelector('.input_lastname').value,
-         email = document.querySelector('.input_email').value;
-    
-         
+
+    checkEmpty(name, lastName, email){
+        let result;
+        if(name === '' || lastName === '' || email === ''){
+            result = false;
+        } else {
+            result = true;
+        };
+        return result;
     };
+
+
+    showFeedback(text, type){
+
+        if( type === 'success'){
+            feedBack.classList.add('success');
+            feedBack.innerText = text;
+            this.removeAlert('success');
+
+        } else if ( type === 'error'){
+            feedBack.classList.add('error');
+            feedBack.innerText = text;
+            this.removeAlert('error');
+        }
+    };
+
+    removeAlert(type){
+        setTimeout(function(){
+            feedBack.classList.remove(type);
+        },3000);
+    };
+    //3뒤 에러메세지 사라짐
+
 };
 
+
+function Customer (name, lastName, email){
+    this.name = name,
+    this.lastName = lastName,
+    this.email = email;
+};
 
 
 
@@ -56,10 +90,27 @@ navBtn.addEventListener('click', () =>ui.toggleMenu());
 // video switch
 switchV.addEventListener('click', () => ui.videoControlls());
 
-drinkForm.addEventListener('submit',function(e){
+// drinkForm
+drinkForm.addEventListener('submit', function(e){
     e.preventDefault();
-    ui.freeDrink();
+
+
+const name = document.querySelector('.input_name').value,
+lastName = document.querySelector('.input_lastname').value,
+ email = document.querySelector('.input_email').value;
+
+    let value =  ui.checkEmpty(name, lastName, email) 
+    if(value){
+        
+       ui.showFeedback('customer added to the list','success');
+
+    } else {
+       ui.showFeedback('some form value empty', 'error');
+       
+    };
+
 });
+
 
 };
 
